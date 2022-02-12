897407ad3cc36fc895199d90b02450ff8cbbf467
fernandobouchet <fernandobouchet@gmail.com>
2022-02-12 18:15:20 -0300

    Fix 'fixQuotes' function

--- src/components/Utils.js
+++ src/components/Utils.js
@@ -1,6 +1,4 @@
 export default function fixQuotes(string) {
-  const result = string
-    .replace(/(&quot;)|(&#039;)/g, '"')
-    .replace(/(&rsquo;)/g, "'");
+  const result = string.replace(/(&quot;)/g, '"').replace(/(&#039;)/g, "'");
   return result;
 }
