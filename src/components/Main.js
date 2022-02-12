e482456e9eb9942afb6a18720540b4db0bc8c28a
fernandobouchet <fernandobouchet@gmail.com>
2022-02-11 23:57:18 -0300

    Add Main start page

---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+export default function Main() {
+  return (
+    <>
+      <h1 className="main-tittle">Quizzical</h1>
+      <h2 className="main-description">Some description if needed</h2>
+      <button className="main-start-button">Start Quiz</button>
+    </>
+  );
+}
