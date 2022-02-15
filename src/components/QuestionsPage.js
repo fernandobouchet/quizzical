464fe7088ef4fb738b028f8d3dac7406c61c550e
fernandobouchet <fernandobouchet@gmail.com>
2022-02-15 00:29:03 -0300

    Move bottom of questions page to a separate component

--- src/components/QuestionsPage.js
+++ src/components/QuestionsPage.js
@@ -1,19 +1,11 @@
-import { useState } from "react";
 import QuestionsCard from "./QuestionsCard";
 import fixQuotes from "./Utils";
 import { nanoid } from "nanoid";
 import "./QuestionsPage.css";
+import QuestionsPageBottom from "./QuestionsPageBottom";
 
 export default function QuestionsPage(props) {
-  const [correctAnswers, setcorrectAnswers] = useState([]);
-
-  const [checkAnswer, setCheckAnswer] = useState(false);
-
   const savedScore = [];
-
-  function updateScore(answers) {
-    setcorrectAnswers(answers);
-  }
 
   function saveScore(answerToInsert) {
     if (!savedScore.includes(answerToInsert)) {
@@ -33,38 +25,13 @@ export default function QuestionsPage(props) {
     );
   });
 
-  function handleAnswer() {
-    setCheckAnswer(true);
-    updateScore(savedScore);
-  }
-
-  function handlePlayAgain() {
-    props.restartGame();
-    setCheckAnswer(false);
-    setcorrectAnswers([]);
-  }
-
   return (
     <div className="question-page-main">
       <div className="questions-container">{questionCards}</div>
-      <div className="bottom-questions-page">
-        {!checkAnswer ? (
-          <>
-            <button className="page-question-buttons" onClick={handleAnswer}>
-              Check answers
-            </button>
-          </>
-        ) : (
-          <div className="score-container">
-            <h2 className="score-result">
-              You scored {correctAnswers.length}/5 correct answers
-            </h2>
-            <button className="page-question-buttons" onClick={handlePlayAgain}>
-              Play again
-            </button>
-          </div>
-        )}
-      </div>
+      <QuestionsPageBottom
+        savedScore={savedScore}
+        restartGame={props.restartGame}
+      />
     </div>
   );
 }
---
+++ src/components/QuestionsPageBottom.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+export default function QuestionsPageBottom(props) {
+  const [correctAnswers, setcorrectAnswers] = useState([]);
+
+  const [checkAnswer, setCheckAnswer] = useState(false);
+
+  function handleAnswer() {
+    setCheckAnswer(true);
+    setcorrectAnswers(props.savedScore);
+  }
+
+  function handlePlayAgain() {
+    props.restartGame();
+    setCheckAnswer(false);
+    setcorrectAnswers([]);
+  }
+
+  return (
+    <div className="bottom-questions-page">
+      {!checkAnswer ? (
+        <>
+          <button className="page-question-buttons" onClick={handleAnswer}>
+            Check answers
+          </button>
+        </>
+      ) : (
+        <div className="score-container">
+          <h2 className="score-result">
+            You scored {correctAnswers.length}/5 correct answers
+          </h2>
+          <button className="page-question-buttons" onClick={handlePlayAgain}>
+            Play again
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
